005c76bb24aa0acc27ffd10ad6b0693a41c56eec
Clayt <clayton.wwjd@gmail.com>
2022-07-05 10:01:46 -0500

    Implemented player object with the ability to follow jump to whatever location the cursor selects on the screen

--- app/src/main/java/com/example/onians/GameView.kt
+++ app/src/main/java/com/example/onians/GameView.kt
@@ -1,12 +1,21 @@
 package com.example.onians
+
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 class GameView(context: Context,attributes: AttributeSet) : SurfaceView(context, attributes), SurfaceHolder.Callback {
     private val thread: GameThread
+    private var player: Player? = null
+
+    private var touched: Boolean = false
+    private var touched_x: Int = 0
+    private var touched_y: Int = 0
 
     init {
         // add callback
@@ -31,6 +40,9 @@ class GameView(context: Context,attributes: AttributeSet) : SurfaceView(context,
     }
 
     override fun surfaceCreated(p0: SurfaceHolder) {
+        // game objects
+        player = Player(BitmapFactory.decodeResource(resources, R.drawable.player_smoke))
+
         // start the game thread
         thread.setRunning(true)
         thread.start()
@@ -43,7 +55,9 @@ class GameView(context: Context,attributes: AttributeSet) : SurfaceView(context,
      * Function to update positions of player and game objects
      */
     fun update(){
-
+        if(touched) {
+            player!!.updateTouch(touched_x, touched_y)
+        }
     }
 
     /**
@@ -51,5 +65,25 @@ class GameView(context: Context,attributes: AttributeSet) : SurfaceView(context,
      */
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
+
+        player!!.draw(canvas)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // whenever there is a touch on the screen
+        // we can get the position of the touch
+        // which we may use for tracking some of the game objects
+        touched_x = event.x.toInt()
+        touched_y = event.y.toInt()
+
+        val action = event.action
+        when (action) {
+            MotionEvent.ACTION_DOWN -> touched = true
+            MotionEvent.ACTION_MOVE -> touched = true
+            MotionEvent.ACTION_UP -> touched = false
+            MotionEvent.ACTION_CANCEL -> touched = false
+            MotionEvent.ACTION_OUTSIDE -> touched = false
+        }
+        return true
     }
 }
